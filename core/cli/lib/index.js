8d81c4b7290be10fea99c2016105deb5b47417f7
minmin <1056640663@qq.com>
2022-06-04 22:51:24 +0800

    feat: 检查node 版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 module.exports = core;
+
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
 const log = require('@snowlepoard520/log');
+const constant = require('./const');
 
 
 function core() {
   checkPkgVersion();
+  checkNodeVersion();
 }
 
 
@@ -14,6 +19,20 @@ function checkPkgVersion() {
   // TODO
   console.log( '版本号 ：', pkg.version);
   log.success('test', 'success...');
+}
+
+
+function checkNodeVersion() {
+  // 获取当前node版本号
+  console.log(process.version);
+  const currentVersion = process.version;
+  // 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if(semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`beibei-cli 需要安装${lowestVersion}以上版本的Node.js`))
+  } else {
+    throw new Error(colors.red(`beibei-cli 需要安装${lowestVersion}以下版本的Node.js`))
+  }
 }
 
 
