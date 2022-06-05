c2b0c6d77eb09754a8339b52fd721a2671ce1f5f
minmin <1056640663@qq.com>
2022-06-05 15:54:06 +0800

    feat: get npmInfoUrl

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -1,12 +1,24 @@
 'use strict';
 
-module.exports = getNpmInfo;
+const axios = require('axios');
+const urlJoin = require('url-join');
+const semver = require('semver');
 
 function getNpmInfo(npmName, registry) {
   console.log(npmName, 'npmName');
+  if(!npmName) {
+    return null;
+  }
+  const registryUrl = registry || getDefaultRegistry();
+  const npmInfoUrl = urlJoin(registryUrl, npmName);
+  console.log(npmInfoUrl, 'npmInfoUrl');
 }
 
+function getDefaultRegistry(isOriginal = false) {
+  return isOriginal ? 'https://registry.npmjs.org/': 'https://registry.npm.taobao.org/'
+}
 
 module.exports = {
   getNpmInfo
 };
+
