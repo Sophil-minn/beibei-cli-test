62974432280d6c48d7aeca15913e5054add1439d
minmin <1056640663@qq.com>
2022-06-08 22:29:22 +0800

    feat: 创建npm 通用类 Package

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const Package = require('@snowlepoard520/package');
 
 function exec() {
+  const pkg = new Package();
+  console.log(pkg, 1234);
   console.log('exec', 111);
 }
 
---
+++ models/package/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+class Package {
+  constructor() {
+    console.log('初始化 package constructor');
+  }
+}
+
+module.exports = Package;
